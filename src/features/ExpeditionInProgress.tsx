2a06f3812a0d2ac748cd84246356c92baebc0f77
pyeonh8 <pyeonh8@gmail.com>
2025-12-28 22:04:35 +0900

    feat: 타이머 완료 후 모달

--- src/features/ExpeditionInProgress.tsx
+++ src/features/ExpeditionInProgress.tsx
@@ -1,36 +1,60 @@
-// import { ExpeditionInProgressProps } from '@/types/features.type';
-// import useTimer from '@/shared/hooks/useTimer';
-// import { useEffect } from 'react';
-// import Modal from '@/shared/ui/modal/Modal';
+import { ExpeditionInProgressProps } from '@/types/features.type';
+import useTimer from '@/shared/hooks/useTimer';
+import { useEffect, useState } from 'react';
+import Modal from '@/shared/ui/modal/Modal';
+import ModalButton from '@/shared/ui/modal/ModalButton';
 
-// // 탐험 진행 화면
-// const ExpeditionInProgress = ({
-//   timerTime,
-//   onStart,
-//   isStarted,
-// }: ExpeditionInProgressProps) => {
-//   const handleTimerEnd = () => {
-//     <Modal>테스트</Modal>;
-//   };
+// 탐험 진행 화면
+const ExpeditionInProgress = ({
+  timerTime,
+  onStart,
+  isStarted,
+}: ExpeditionInProgressProps) => {
+  const [modalOpen, setModalOpen] = useState(false);
 
-//   const { timerCount, isRunning, start, pause, reset } = useTimer({
-//     initialValue: Number(timerTime),
-//     delay: 1000,
-//     onComplete: handleTimerEnd,
-//   });
+  const handleTimerEnd = () => {
+    setModalOpen(true);
+  };
 
-//   useEffect(() => {
-//     if (isStarted) start();
-//   }, [isStarted, start]);
+  const { timerCount, isRunning, start, pause } = useTimer({
+    initialValue: Number(timerTime),
+    delay: 1000,
+    onComplete: handleTimerEnd,
+  });
 
-//   return (
-//     <>
-//       <div>탐험 진행 화면</div>
-//       <div>
-//         <div>카운터 시간 {timerCount}</div>
-//         <p>상태: {isRunning ? '실행 중' : '정지됨'}</p>
-//       </div>
-//     </>
-//   );
-// };
-// export default ExpeditionInProgress;
+  useEffect(() => {
+    if (isStarted) start();
+  }, [isStarted, start]);
+
+  return (
+    <>
+      {/* 아... 모달 ... 외부 클릭 되는 거 막아야함... */}
+      <Modal
+        isOpen={modalOpen}
+        hideCloseButton={true}
+        actionButton={
+          <ModalButton
+            onClick={() => {
+              setModalOpen(false);
+              onStart(false);
+            }}
+          >
+            되돌아가기
+          </ModalButton>
+        }
+      >
+        모험이 끝났습니다.
+      </Modal>
+      <div>탐험 진행 화면</div>
+      <div>
+        <div>카운터 시간 {timerCount}</div>
+        <p>상태: {isRunning ? '실행 중' : '정지됨'}</p>
+        <button onClick={pause}>중지</button>
+      </div>
+
+      {/* 임시 */}
+      <button onClick={() => onStart(false)}>탐험 준비하러 가기</button>
+    </>
+  );
+};
+export default ExpeditionInProgress;
