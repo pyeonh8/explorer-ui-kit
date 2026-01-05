dd633742be38c9bdfba25599f4f6c324cd49fef9
pyeonh8 <pyeonh8@gmail.com>
2026-01-05 22:05:48 +0900

    style: 캐릭터 화면 배경 추가

--- src/features/characterPanel/CharacterPanel.tsx
+++ src/features/characterPanel/CharacterPanel.tsx
@@ -11,9 +11,12 @@ const CharacterPanel = ({
   const villagerMap = new Map(villagers.map((v) => [v.name, v]));
 
   return (
-    <div className="bg-amber-100 p-[50px]">
-      캐릭터 창
-      <div className="flex gap-4">
+    <div className="relative min-h-[200px] w-full overflow-hidden rounded-[10px] bg-[linear-gradient(0deg,rgba(146,218,205,1)_0%,rgba(192,240,183,1)_50%,rgba(255,255,173,1)_100%)]">
+      {/* 배경 */}
+      <div className="absolute inset-0 scale-105 bg-[url('/images/character-bg.jpg')] bg-cover bg-center opacity-80 blur-[4px]"></div>
+
+      {/* 캐릭터 */}
+      <div className="relative z-10 flex gap-4">
         {selectedAmiibo?.map((v) => {
           const target = villagerMap.get(v);
 
@@ -26,12 +29,6 @@ const CharacterPanel = ({
                     alt={target.name}
                     width={60}
                     height={80}
-                    // style={{
-                    //   width: 'auto',
-                    //   height: 'auto',
-                    //   maxHeight: '100%',
-                    // }}
-                    // className="object-contain"
                   />
                 )}
               </div>
