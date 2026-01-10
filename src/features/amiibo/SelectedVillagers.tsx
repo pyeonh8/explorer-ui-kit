bf3833637a56a9fa5396fe68c960ebee98d3a5b0
pyeonh8 <pyeonh8@gmail.com>
2026-01-10 19:09:45 +0900

    style: npc 말풍선 UI 스타일링 추가

--- src/features/amiibo/SelectedVillagers.tsx
+++ src/features/amiibo/SelectedVillagers.tsx
@@ -1,54 +1,47 @@
 import Image from 'next/image';
-import { villagers, npcs } from 'animal-crossing';
+import { villagers } from 'animal-crossing';
 import { AmiiboSelectionProps } from '@/types/features.type';
 import { MdCancel } from 'react-icons/md';
 import InfoBubble from '@/shared/ui/InfoBubble';
 
 const villagerMap = new Map(villagers.map((v) => [v.name, v]));
-const isabelle = npcs.find((n) => n.name.toLocaleLowerCase() === 'isabelle');
 
 const SelectedVillagers = ({
   selectedAmiibo,
   onSelect,
 }: AmiiboSelectionProps) => {
   return (
-    <div className="flex min-h-[100px] items-center justify-center gap-4">
+    <div className="my-2 flex min-h-[76px] justify-center">
       {selectedAmiibo.length > 0 ? (
-        selectedAmiibo?.map((v) => {
-          const target = villagerMap.get(v);
-          if (!target?.iconImage) return null;
+        <div className="flex items-center justify-center gap-4">
+          {selectedAmiibo?.map((v) => {
+            const target = villagerMap.get(v);
+            if (!target?.iconImage) return null;
 
-          return (
-            <button
-              key={target.filename}
-              onClick={() => {
-                onSelect(target.name);
-              }}
-              className="group relative flex cursor-pointer flex-col items-center rounded-2xl bg-(--color-secondary) p-1"
-            >
-              <MdCancel className="absolute top-1.5 right-1.5 rounded-2xl bg-white text-[15px] text-(--color-accent)/80 opacity-0 transition-all group-hover:opacity-100" />
-              <Image
-                src={target.iconImage}
-                alt={target.name}
-                height={50}
-                width={50}
-              />
-              <span className="relative -top-1 text-[12px] font-bold">
-                {target.translations.kRko}
-              </span>
-            </button>
-          );
-        })
+            return (
+              <button
+                key={target.filename}
+                onClick={() => {
+                  onSelect(target.name);
+                }}
+                className="group relative flex cursor-pointer flex-col items-center rounded-2xl bg-(--color-secondary) p-1"
+              >
+                <MdCancel className="absolute top-1.5 right-1.5 rounded-2xl bg-white text-[15px] text-(--color-accent)/80 opacity-0 transition-all group-hover:opacity-100" />
+                <Image
+                  src={target.iconImage}
+                  alt={target.name}
+                  height={50}
+                  width={50}
+                />
+                <span className="relative -top-1 text-[12px] font-bold">
+                  {target.translations.kRko}
+                </span>
+              </button>
+            );
+          })}
+        </div>
       ) : (
         <>
-          {isabelle?.iconImage && (
-            <Image
-              src={isabelle.iconImage}
-              alt={isabelle.name}
-              height={55}
-              width={55}
-            />
-          )}
           <InfoBubble>함께 모험을 떠날 친구들을 선택해주세요!</InfoBubble>
         </>
       )}
--- src/shared/ui/InfoBubble.tsx
+++ src/shared/ui/InfoBubble.tsx
@@ -1,11 +1,28 @@
 import { InfoBubbleProps } from '@/types/common.types';
+import { npcs } from 'animal-crossing';
+import Image from 'next/image';
 import { twMerge } from 'tailwind-merge';
+
+const isabelle = npcs.find((n) => n.name.toLocaleLowerCase() === 'isabelle');
 
 const InfoBubble = ({ children = '', className = '' }: InfoBubbleProps) => {
   const baseStyle =
-    "relative rounded-3xl bg-[#fff4c5] px-6 py-3 text-[17px] after:absolute after:top-1/2 after:-left-6 after:-translate-y-1/2 after:border-12 after:border-r-20 after:border-transparent after:border-r-[#fff4c5] after:content-[''] sm:py-5";
+    "relative rounded-3xl bg-[#fff4c5] px-6 py-3 text-[17px] after:absolute after:top-1/2 after:-left-6 after:-translate-y-1/2 after:border-12 after:border-r-20 after:border-transparent after:border-r-[#fff4c5] after:content-[''] sm:py-4";
   const finalClasses = twMerge(baseStyle, className);
-  return <div className={finalClasses}>{children}</div>;
+
+  return (
+    isabelle?.iconImage && (
+      <div className="flex items-center justify-center gap-4">
+        <Image
+          src={isabelle.iconImage}
+          alt={isabelle.name}
+          height={55}
+          width={55}
+        />
+        <div className={finalClasses}>{children}</div>
+      </div>
+    )
+  );
 };
 
 export default InfoBubble;
