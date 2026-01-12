12e6a70a42f2f4c02efcda77dd46af7d551e5c59
pyeonh8 <pyeonh8@gmail.com>
2026-01-12 14:00:08 +0900

    fix: 이미지 오류 해결 2

--- src/features/amiibo/AmiiboCard.tsx
+++ src/features/amiibo/AmiiboCard.tsx
@@ -25,7 +25,7 @@ const AmiiboCard = ({ amiibo, isSelected, onSelect }: AmiiboCardProps) => {
           src={amiibo.image}
           alt={amiibo.character}
           fill={true}
-          sizes="(max-width: 640px) 50vw, (max-width: 1024px) 33vw, 20vw"
+          sizes="(max-width: 768px) 33vw, 25vw"
           className="object-contain"
         />
       </div>
