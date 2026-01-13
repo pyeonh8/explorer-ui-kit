92ec9339acf3f097605282b19cff7e9b70fab15f
pyeonh8 <pyeonh8@gmail.com>
2026-01-13 20:00:19 +0900

    refactor: 타이머 완료 로그 뽀모도로 타이머 컴포넌트로 이전

--- src/features/characterPanel/CharacterPanel.tsx
+++ src/features/characterPanel/CharacterPanel.tsx
@@ -20,7 +20,7 @@ const CharacterPanel = ({
   );
 
   return (
-    <div className="relative h-[150px] w-[full] overflow-hidden rounded-[10px] sm:h-[200px]">
+    <div className="relative h-[150px] w-[full] overflow-hidden rounded-[10px] sm:h-[170px]">
       {/* 배경 */}
       <div className="absolute inset-0 scale-105 bg-[url('/images/character-bg01.jpg')] bg-cover bg-center opacity-80 blur-[3px]"></div>
 
@@ -36,7 +36,7 @@ const CharacterPanel = ({
           return (
             <div
               key={target?.id}
-              className="relative bottom-3 flex items-end sm:bottom-7"
+              className="relative bottom-3 flex items-end sm:bottom-4.5"
               style={{
                 height: `${height}px`,
                 width: 'auto',
